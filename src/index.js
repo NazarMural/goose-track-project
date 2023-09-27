17c13993c3ea90b51c4c5b091d6e592ab2948f50
Alona Kuznietsova <acvetochka@gmail.com>
2023-09-27 23:48:06 +0200

    add fonts and variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import 'normalize.css';
 import './index.css';
+import './styles/variables.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
