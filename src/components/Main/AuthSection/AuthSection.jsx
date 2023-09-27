7f99719b3e69d82290bb247acb2124f8a2e48888
Olha Tverdokhlib <balabolya@gmail.com>
2023-09-28 00:20:24 +0200

    first commit

---
+++ src/components/Main/AuthSection/AuthSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AuthSectionContainer } from './AuthSection.styled';
+
+
+const AuthSection = () => {
+    return (
+      <AuthSectionContainer>
+        <h1>GooseTrack</h1>
+      </AuthSectionContainer>
+    );
+};
+
+export default AuthSection;
---
+++ src/components/Main/AuthSection/AuthSection.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const AuthSectionContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #3e85f3;
+`;
