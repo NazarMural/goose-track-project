4c271023adb762c7b00d9fd3350366eb952050d9
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-09-27 16:53:42 +0300

    upd auth operations

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,6 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+axios.defaults.baseURL = 'https://goose-track-project-backend.onrender.com';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
 const signUpOperation = createAsyncThunk('auth/signup', async (credentials, thunkAPI) => { })
 const signInOperation = createAsyncThunk('auth/signin', async (credentials, thunkAPI) => { })
 const logOutOperation = createAsyncThunk('auth/logout', async (_, thunkAPI) => { })
