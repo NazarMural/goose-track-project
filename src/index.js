30c07e742df274ac0307a3b0e14fa4331c096c88
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-09-28 20:35:09 +0300

    upd index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 import { App } from 'components/App';
+import 'normalize.css';
 import './index.css';
+import './styles/variables.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
