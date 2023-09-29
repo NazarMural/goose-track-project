131f5c950bc5bcc735331cb9e3987609cef34445
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-09-29 15:09:53 +0300

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,4 +1,5 @@
 const selectUser = state => state.auth.user;
+const selectToken = state => state.auth.token;
 const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
-export { selectIsLoggedIn, selectUser };
+export { selectIsLoggedIn, selectUser, selectToken };
