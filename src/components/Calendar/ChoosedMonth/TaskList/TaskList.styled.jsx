492655a2ff148fbd224d4efa70136f0e5a52f9d3
Alona Kuznietsova <acvetochka@gmail.com>
2023-09-30 22:49:33 +0200

    add MainContainer

--- src/components/Calendar/ChoosedMonth/TaskList/TaskList.styled.jsx
+++ src/components/Calendar/ChoosedMonth/TaskList/TaskList.styled.jsx
@@ -10,21 +10,27 @@ export const List = styled.ul`
 
 export const TaskItem = styled.li`
   background-color: ${props => props.bg};
+  color: ${props => props.color};
   border-radius: 8px;
   padding: 4px 8px;
   /* display: flex; */
   /* flex-wrap: nowrap; */
   margin-bottom: 8px;
   /* overflow-x: hidden; */
-  white-space: pre-wrap;
+  /* white-space: pre-wrap;
   word-break: break-all;
-  word-wrap: break-word;
+  word-wrap: break-word; */
+  max-width: 44px; /* Ширина контейнера */
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `;
 
 export const TaskLink = styled(NavLink)`
   text-decoration: none;
-  color: ${props => props.color};
-  overflow: hidden;
+  /* color: ${props => props.color}; */
+  color: inherit;
+  /* overflow: hidden;
   text-overflow: ellipsis;
-  white-space: nowrap;
+  white-space: nowrap; */
 `;
--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ChoosedMonth from 'components/Calendar/ChoosedMonth/ChoosedMonth';
+import { MainContainer } from 'components/MainContainer/MainContainer';
 // import { CalendarTable } from 'components/Calendar/ChoosedMonth/CalendarTable/CalendarTable';
 
 const CalendarPage = () => {
-  return <ChoosedMonth />;
+  return (
+    <MainContainer>
+      <ChoosedMonth />
+    </MainContainer>
+  );
 };
 
 export default CalendarPage;
