2783fe429558d3e4d87ca84ff6d8c0d02b881389
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-10-01 22:28:41 +0300

    Розкоментував маршрути

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,19 +17,19 @@ export const App = () => {
     <Suspense fallback={null}>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        {/* <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/user" element={<MainLayout />}>
           <Route path="account" element={<AccountPage />} />
-          {/* <Route path="calendar" element={<CalendarPage />} />
+          <Route path="calendar" element={<CalendarPage />} />
           <Route path="calendar/day/:currentDay" element={<ChoosedDay />} />
           <Route
             path="calendar/month/:currentMonth"
             element={<ChoosedMonth />}
           />
-          <Route path="statistics" element={<StatisticsPage />} /> */}
+          <Route path="statistics" element={<StatisticsPage />} />
         </Route>
-        {/* <Route path="*" element={<ErrorPage />} /> */}
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Suspense>
   );
