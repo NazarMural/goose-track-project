d59f7ca5d0f561d3c1919c541067ee73f8d4b5be
NazarMural <nazarmural777@gmail.com>
2023-10-01 15:04:47 +0300

    Додає графік

--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import StatisticsСhart from 'components/Statistics/StatisticsChart/StatisticsChart';
+import { MainContainer } from 'components/MainContainer/MainContainer';
 
 const StatisticsPage = () => {
   return (
-    <>
+    <MainContainer>
       <StatisticsСhart />
-    </>
+    </MainContainer>
   );
 };
 
