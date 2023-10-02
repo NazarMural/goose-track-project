f1289cae893342710c496821bc1f4f575faa38b3
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-02 21:02:49 +0300

    Update CalendarPage.jsx

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ChoosedMonth from 'components/Calendar/ChoosedMonth/ChoosedMonth';
 import { MainContainer } from 'components/MainContainer/MainContainer';
-import ChoosedDay from 'components/Calendar/ChoosedDay/ChoosedDay';
-// import { CalendarTable } from 'components/Calendar/ChoosedMonth/CalendarTable/CalendarTable';
 
 const CalendarPage = () => {
   return (
     <MainContainer>
-      {/* <ChoosedMonth /> */}
-      <ChoosedDay />
+      <ChoosedMonth />
     </MainContainer>
   );
 };
