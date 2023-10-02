62c60beaa841a3094edc8680f90a5d9e88a2eb00
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-10-02 10:44:16 +0300

    other_fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,20 +16,17 @@ export const App = () => {
   return (
     <Suspense fallback={null}>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<MainPage />} />{' '}
-        </Route>
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/calendar" element={<CalendarPage />}>
-          <Route path="calendar/day/:currentDay" element={<ChoosedDay />} />
-          <Route
-            path="calendar/month/:currentMonth"
-            element={<ChoosedMonth />}
-          />
-        </Route>
-        <Route path="/statistics" element={<StatisticsPage />} />
+        <Route index element={<MainPage />} />{' '}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/calendar" element={<CalendarPage />}>
+            <Route path="day/:currentDay" element={<ChoosedDay />} />
+            <Route path="month/:currentMonth" element={<ChoosedMonth />} />
+          </Route>
+          <Route path="/statistics" element={<StatisticsPage />} />
+        </Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Suspense>
