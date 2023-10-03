6946ce7e23d3f50ea2e7dbd40c1b0383c9e878d6
acvetochka <116402791+acvetochka@users.noreply.github.com>
2023-10-03 11:26:54 +0000

    Deploying to gh-pages from @ NazarMural/goose-track-project@51fd45a9cecceb291ffb4051b9e4339f6937e1b1 🚀

--- static/js/149.a9e4c095.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgoose_track_project=self.webpackChunkgoose_track_project||[]).push([[149],{7149:function(c,e,s){c.exports=s.p+"static/media/icons.128c807bc0e6443fa2fd.svg"}}]);
