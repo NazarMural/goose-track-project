f78db937df23d3a807ad420de844d101ee5641c7
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-04 17:37:36 +0300

    Розділив код на компоненти

---
+++ src/components/Calendar/ChoosedDay/Title/Title.styled.jsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import { min, tablet } from 'styles/media';
+
+export const ContainerTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+  ${min(tablet)} {
+    margin-bottom: 28px;
+  }
+`;
+
+export const MainTitle = styled.h2`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 20px;
+  margin: 0;
+
+  color: var(--primary-text-color);
+  ${min(tablet)} {
+    font-size: 20px;
+    line-height: 24px;
+  }
+`;
+
+export const IconAddTask = styled.svg`
+  height: 22px;
+  width: 22px;
+  stroke: var(--primary-text-color);
+  padding: 7px 0;
+  cursor: pointer;
+  & use {
+    fill: transparent;
+  }
+  ${min(tablet)} {
+    height: 24px;
+    width: 24px;
+  }
+`;
