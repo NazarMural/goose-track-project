654a86541ca41e95ffc256ff4ea3b0fa167691ca
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-07 19:42:45 +0300

    fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,5 @@
 import AuthForm from 'components/AuthForm/AuthForm';
 import AuthLayout from 'components/AuthLayout/AuthLayout';
-import { LinkStyled } from 'components/AuthLayout/AuthLayout.styled';
 import React from 'react';
 
 const LoginPage = () => {
