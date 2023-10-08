fb9e9948e6b5e15dab486bed8f5d834c89e9b7a0
Alona Kuznietsova <acvetochka@gmail.com>
2023-10-08 17:57:07 +0200

    change calendar-navigate

--- src/components/Calendar/ChoosedDay/Head/Head.jsx
+++ src/components/Calendar/ChoosedDay/Head/Head.jsx
@@ -40,7 +40,10 @@ const Head = () => {
   return (
     <HeadContainer isSelected={isSelected}>
       {[...Array(7)].map((_, idx) => (
-        <HeadCell onClick={() => onChangeDay(moment().day(idx + 1))} key={idx}>
+        <HeadCell
+          onClick={() => onChangeDay(moment(currentDay).day(idx + 1))}
+          key={idx}
+        >
           <NameWeek>
             {isMobile
               ? moment()
