55894e11a54b3b66ba2cc00824f764b8a63f7846
Alona Kuznietsova <acvetochka@gmail.com>
2023-10-09 16:29:52 +0200

    fixed-bugs clear-code

--- src/components/Calendar/CalendarToolbar/CalendarToolbar.styled.jsx
+++ src/components/Calendar/CalendarToolbar/CalendarToolbar.styled.jsx
@@ -11,54 +11,54 @@ export const Toolbar = styled.div`
   }
 `;
 
-export const DateContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 18px;
-  gap: 8px;
-`;
+// export const DateContainer = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   margin-bottom: 18px;
+//   gap: 8px;
+// `;
 
-export const DateWrapper = styled.div`
-  background-color: var(--accent-color);
-  width: 150px;
-  /* width: auto; */
-  display: flex;
-  padding: 6px 12px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-`;
+// export const DateWrapper = styled.div`
+//   background-color: var(--accent-color);
+//   width: 150px;
+//   /* width: auto; */
+//   display: flex;
+//   padding: 6px 12px;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 8px;
+// `;
 
-export const Date = styled.span`
-  text-transform: uppercase;
-  color: var(--invert-text-color);
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 18px;
-`;
+// export const Date = styled.span`
+//   text-transform: uppercase;
+//   color: var(--invert-text-color);
+//   font-size: 14px;
+//   font-style: normal;
+//   font-weight: 700;
+//   line-height: 18px;
+// `;
 
-export const ToggleWrapper = styled.div`
-  /* padding: 7px 10px; */
-  display: flex;
-  background: var(--calendar-grid-border);
-  border: 1px solid var(--calendar-grid-border);
-  border-radius: 8px;
-  gap: 1px;
-  overflow: hidden;
-`;
+// export const ToggleWrapper = styled.div`
+//   /* padding: 7px 10px; */
+//   display: flex;
+//   background: var(--calendar-grid-border);
+//   border: 1px solid var(--calendar-grid-border);
+//   border-radius: 8px;
+//   gap: 1px;
+//   overflow: hidden;
+// `;
 
-export const Toggle = styled.div`
-  padding: 7px 10px;
-  background: var(--primary-bg-color);
-`;
+// export const Toggle = styled.div`
+//   padding: 7px 10px;
+//   background: var(--primary-bg-color);
+// `;
 
-export const ToggleIcon = styled.svg`
-  fill: var(--chevron-btn-color);
-  &:hover {
-    fill: var(--accent-color);
-  }
-`;
+// export const ToggleIcon = styled.svg`
+//   fill: var(--chevron-btn-color);
+//   &:hover {
+//     fill: var(--accent-color);
+//   }
+// `;
 
 export const TypeWrapper = styled.div`
   display: flex;
@@ -90,5 +90,3 @@ export const TypeLink = styled(NavLink)`
     background-color: var(--switch-type-bg-active);
   }
 `;
-
-
--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -6,11 +6,7 @@ import moment from 'moment';
 
 const CalendarPage = () => {
   const navigate = useNavigate();
-  // const [currentDate, setCurrentDate] = useState(
-  //   localStorage.getItem('date') || moment().format('YYYY-MM-DD').toString()
-  // );
-  // const [format, setFormat] = useState(localStorage.getItem('type') || 'month');
-  // let format = ;
+
   useEffect(() => {
     const month = moment().format('YYYY-MM').toString();
     navigate(`/calendar/month/${month}`);
@@ -33,37 +29,9 @@ const CalendarPage = () => {
     localStorage.setItem('type', format);
   }, [navigate, format]);
 
-  // useEffect(() => {
-  //   let date;
-  //   const format = localStorage.getItem('type') || 'month';
-  //   switch (format) {
-  //     case 'month':
-  //       date = moment(
-  //         localStorage.getItem('date') ||
-  //           moment().format('YYYY-MM-DD').toString()
-  //       ).format('YYYY-MM');
-  //       navigate(`/calendar/month/${date}`);
-  //       break;
-  //     case 'day':
-  //       date = moment(
-  //         localStorage.getItem('date') ||
-  //           moment().format('YYYY-MM-DD').toString()
-  //       ).format('YYYY-MM-DD');
-  //       navigate(`/calendar/day/${date}`);
-  //       break;
-  //     default:
-  //       return;
-  //   }
-  // }, [navigate]);
-
   return (
     <MainContainer>
-      <CalendarToolbar
-      // currentDate={currentDate}
-      // setCurrentDate={setCurrentDate}
-      // globalFormat={format}
-      // setFormat={setFormat}
-      />
+      <CalendarToolbar />
       <Outlet />
     </MainContainer>
   );
