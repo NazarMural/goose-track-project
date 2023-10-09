50326d75653ab65b690ce9f16d5ed756a71e368d
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-10-10 00:27:42 +0300

    prtt 200

--- src/components/TaskModal/TaskForm/TaskForm.jsx
+++ src/components/TaskModal/TaskForm/TaskForm.jsx
@@ -1,16 +1,10 @@
-import React, {
-  useState,
-  useEffect,
-} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Formik, Form } from 'formik';
 import { useDispatch } from 'react-redux';
 import icons from '../../../assets/images/icons/icons.svg';
 import * as Yup from 'yup';
 
-import {
-  addTaskOperation,
-  updateTaskOperation,
-} from '../../../redux/tasks/operations';
+import { addTaskOperation, updateTaskOperation } from '../../../redux/tasks/operations';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {
   OperationButton,
@@ -39,42 +33,25 @@ import {
 import { TaskValidateMessage } from '../TaskValidateMessage/TaskValidateMessage';
 
 const taskFormSchema = Yup.object().shape({
-  title: Yup.string('Enter title')
-    .max(
-      250,
-      'Text must be at most 250characters'
-    )
-    .required('Title is required'),
+  title: Yup.string('Enter title').max(250, 'Text must be at most 250characters').required('Title is required'),
   start: Yup.string('Enter start')
-    .matches(
-      /^([01]\d|2[0-3]):([0-5]\d)$/,
-      'Invalid start time'
-    )
+    .matches(/^([01]\d|2[0-3]):([0-5]\d)$/, 'Invalid start time')
     .required('Start time is required'),
   end: Yup.string()
-    .matches(
-      /^([01]\d|2[0-3]):([0-5]\d)$/,
-      'Invalid end time'
-    )
+    .matches(/^([01]\d|2[0-3]):([0-5]\d)$/, 'Invalid end time')
     .when('start', (start, schema) =>
       schema.test({
         test: function (end) {
           if (!start || !end) return true;
           return end > start;
         },
-        message:
-          'End time must be greater than start time',
+        message: 'End time must be greater than start time',
       })
     ),
 });
 
-export const TaskForm = ({
-  category,
-  task = {},
-  onClose,
-}) => {
-  const [operation, setOperation] =
-    useState('create');
+export const TaskForm = ({ category, task = {}, onClose }) => {
+  const [operation, setOperation] = useState('create');
 
   const dispatch = useDispatch();
 
@@ -123,9 +100,7 @@ export const TaskForm = ({
           if (data.error) {
             throw new Error(data.payload);
           }
-          Notify.success(
-            'Task has been successfully created.'
-          );
+          Notify.success('Task has been successfully created.');
           onClose();
         })
         .catch(error => {
@@ -145,139 +120,69 @@ export const TaskForm = ({
     >
       {({ values, errors, touched }) => (
         <FormContainer>
-          <CloseButton
-            type="button"
-            aria-label="close button"
-            onClick={onClose}
-          >
+          <CloseButton type="button" aria-label="close button" onClick={onClose}>
             <CloseIcon>
-              <use
-                href={icons + '#icon-x-close'}
-              ></use>
+              <use href={icons + '#icon-x-close'}></use>
             </CloseIcon>
           </CloseButton>
           <Form>
             <TitleFieldContainer>
               <Label>
                 Title
-                <TitleField
-                  type="text"
-                  name="title"
-                  placeholder="Enter text"
-                />
-                <TaskValidateMessage
-                  errors={errors.title}
-                  touched={touched?.title}
-                  field="title"
-                />
+                <TitleField type="text" name="title" placeholder="Enter text" />
+                <TaskValidateMessage errors={errors.title} touched={touched?.title} field="title" />
               </Label>
             </TitleFieldContainer>
 
             <FieldContainer>
               <Label>
                 Start
-                <TimeField
-                  type="time"
-                  name="start"
-                />
-                <TaskValidateMessage
-                  errors={errors.start}
-                  touched={touched?.start}
-                  field="start time"
-                />
+                <TimeField type="time" name="start" />
+                <TaskValidateMessage errors={errors.start} touched={touched?.start} field="start time" />
               </Label>
               <Label>
                 End
-                <TimeField
-                  type="time"
-                  name="end"
-                />
-                <TaskValidateMessage
-                  errors={errors.end}
-                  touched={touched?.end}
-                  field="end time"
-                />
+                <TimeField type="time" name="end" />
+                <TaskValidateMessage errors={errors.end} touched={touched?.end} field="end time" />
               </Label>
             </FieldContainer>
 
             <PriorityContainer role="group">
               <PriorityLabel>
-                <PriorityField
-                  type="radio"
-                  name="priority"
-                  value="low"
-                />
+                <PriorityField type="radio" name="priority" value="low" />
                 {values.priority === 'low' ? (
                   <BlueLine>
-                    <use
-                      href={
-                        icons +
-                        '#icon-ellipse-blue-stroke'
-                      }
-                    ></use>
+                    <use href={icons + '#icon-ellipse-blue-stroke'}></use>
                   </BlueLine>
                 ) : (
                   <Blue>
-                    <use
-                      href={
-                        icons +
-                        '#icon-ellipse-blue'
-                      }
-                    ></use>
+                    <use href={icons + '#icon-ellipse-blue'}></use>
                   </Blue>
                 )}
                 Low
               </PriorityLabel>
               <PriorityLabel>
-                <PriorityField
-                  type="radio"
-                  name="priority"
-                  value="medium"
-                />
+                <PriorityField type="radio" name="priority" value="medium" />
                 {values.priority === 'medium' ? (
                   <OrangeLine>
-                    <use
-                      href={
-                        icons +
-                        '#icon-ellipse-orange-stroke'
-                      }
-                    ></use>
+                    <use href={icons + '#icon-ellipse-orange-stroke'}></use>
                   </OrangeLine>
                 ) : (
                   <Orange>
-                    <use
-                      href={
-                        icons +
-                        '#icon-ellipse-orange'
-                      }
-                    ></use>
+                    <use href={icons + '#icon-ellipse-orange'}></use>
                   </Orange>
                 )}
                 Medium
               </PriorityLabel>
               <PriorityLabel>
-                <PriorityField
-                  type="radio"
-                  name="priority"
-                  value="high"
-                />
+                <PriorityField type="radio" name="priority" value="high" />
                 {values.priority === 'high' ? (
                   <RedLine>
-                    <use
-                      href={
-                        icons +
-                        '#icon-ellipse-pink-stroke'
-                      }
-                    ></use>
+                    <use href={icons + '#icon-ellipse-pink-stroke'}></use>
                   </RedLine>
                 ) : (
                   <Red>
-                    <use
-                      href={
-                        icons +
-                        '#icon-ellipse-pink'
-                      }
-                    ></use>
+                    <use href={icons + '#icon-ellipse-pink'}></use>
                   </Red>
                 )}
                 High
@@ -287,33 +192,21 @@ export const TaskForm = ({
             <ButtonContainer>
               {operation === 'edit' ? (
                 <OperationButton type="submit">
-                  <EditIcon
-                    stroke="#fff"
-                    fill="none"
-                  >
-                    <use
-                      href={
-                        icons + '#icon-pencil'
-                      }
-                    ></use>
+                  <EditIcon stroke="#fff" fill="none">
+                    <use href={icons + '#icon-pencil'}></use>
                   </EditIcon>
                   Edit
                 </OperationButton>
               ) : (
                 <OperationButton type="submit">
                   <AddIcon stroke="#fff">
-                    <use
-                      href={icons + '#icon-plus'}
-                    ></use>
+                    <use href={icons + '#icon-plus'}></use>
                   </AddIcon>
                   Add
                 </OperationButton>
               )}
 
-              <CancelButton
-                type="button"
-                onClick={onClose}
-              >
+              <CancelButton type="button" onClick={onClose}>
                 Cancel
               </CancelButton>
             </ButtonContainer>
