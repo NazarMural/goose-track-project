798c1c7e4fe015fbe8f510b5af3c4f3fc0cf4b93
Maksym Karalash <Maks.karalash@gmail.com>
2023-10-12 16:48:28 +0300

    fix

--- src/utils/reduxActionHandlers/authActionHandlers/updateAvatarActionHandlers/handleUpdateAvatarFulfilled.js
+++ src/utils/reduxActionHandlers/authActionHandlers/updateAvatarActionHandlers/handleUpdateAvatarFulfilled.js
@@ -1,5 +1,4 @@
 export const handleUpdateAvatarFulfilled = (state, { payload }) => {
-  state.isAuthLoading = false;
   state.isUpdatingAvatar = false;
   state.error = null;
   state.user.avatarURL = payload.file;
--- src/utils/reduxActionHandlers/authActionHandlers/updateAvatarActionHandlers/handleUpdateAvatarPending.js
+++ src/utils/reduxActionHandlers/authActionHandlers/updateAvatarActionHandlers/handleUpdateAvatarPending.js
@@ -1,4 +1,3 @@
 export const handleUpdateAvatarPending = state => {
-  state.isAuthLoading = true;
   state.isUpdatingAvatar = true;
 };
